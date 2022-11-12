3b3070850278981402429d6ab136118519b600cc
ogelbw <ogelbw@gmail.com>
2022-11-12 19:36:36 +0000

    utilities

--- scripts/Utility.js
+++ scripts/Utility.js
@@ -1,3 +1,13 @@
-function getIsWall(x,y){
-    
+function IsWall(x,y){
+    let thing = getNode(x,y)
+    return thing.getAttribute("data") == "wall"
+}
+
+function isGoal(){
+    let thing = getNode(x,y)
+    return thing.getAttribute("data") == "goal"
+}
+
+function getNode(x,y){
+    return document.getElementById("node_"+y+"_"+x)
 }
